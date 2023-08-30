74ff29b79be1bbf4179c8bf7a2d75687387fc0ec
dezeroku <dezeroku@gmail.com>
2023-08-30 21:21:14 +0200

    core: remove the now-obsolete Victoria Metrics

--- helmfile/core/vault-terraform/variables.tf
+++ helmfile/core/vault-terraform/variables.tf
@@ -1,15 +1,3 @@
-variable "victoria_metrics_homekit_token" {
-  type = string
-}
-
-variable "victoria_metrics_test_username" {
-  type = string
-}
-
-variable "victoria_metrics_test_password" {
-  type = string
-}
-
 variable "kube_prometheus_stack_grafana_admin_username" {
   type = string
 }
