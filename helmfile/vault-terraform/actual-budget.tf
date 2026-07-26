4ee42f1d4b84780d4f85c00c9699f183385171b1
dezeroku <contact@dezeroku.com>
2026-07-27 00:34:19 +0200

    actual-budget: migrate to envoy-gateway

--- helmfile/vault-terraform/actual-budget.tf
+++ helmfile/vault-terraform/actual-budget.tf
@@ -3,9 +3,6 @@ module "actual-budget" {
   name               = "actual-budget"
   kubernetes_backend = vault_auth_backend.kubernetes_homeserver.path
 
-  # v5 version of the chart
-  service_account_names = ["actual-budget"]
-
   oidc = {
     redirect_uris = ["https://actual-budget.${var.domain}/openid/callback"]
     group_ids     = [vault_identity_group.this["actual-budget"].id]
